6eb68bc88def4c929ae26579921c72dc8d126708
AdityaKoranga <nishakornga@gmail.com>
2022-07-09 12:08:41 +0530

    made some changes in the script file

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 set -e
 
 #create directory
-cd /home/$USER/
 mkdir -p kyber_dilithium_gen/{kyber512+dilithium2,kyber768+dilithium3,kyber1024+dilithium5_aes}
 
 #now just run the docker command
@@ -12,14 +11,13 @@ docker run -e TEST_TIME=5 -e KEM_ALG=kyber512 -e SIG_ALG=dilithium2 -v /home/$US
 docker run -e TEST_TIME=5 -e KEM_ALG=kyber768 -e SIG_ALG=dilithium3 -v /home/$USER/kyber_dilithium_gen/kyber768+dilithium3:/opt/test/ -it openquantumsafe/curl:optimized perftest.sh
 
 docker run -e TEST_TIME=5 -e KEM_ALG=kyber1024 -e SIG_ALG=dilithium5_aes -v /home/$USER/kyber_dilithium_gen/kyber1024+dilithium5_aes:/opt/test/ -it openquantumsafe/curl:optimized perftest.sh
+
+#All steps done
+
 echo
-echo
-echo
-ls /home/$USER/kyber_dilithium_gen/
 echo
 echo
 echo -e " \033[0;32m -----------SUCCESSFULLY BUILT-----------"
-echo
 echo
 echo
 
